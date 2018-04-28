fff7b09574c74c04dbd14285179d9b0403505417
Dan Zaratsian <dtz001@gmail.com>
2018-04-28 08:28:33 -0400

    Update 05hive_external_table.sh

--- 05hive_external_table.sh
+++ 05hive_external_table.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 cat<<EOF>/tmp/create_table.sql
+#SET hive.druid.metadata.username=${DRUID_USERNAME};
+#SET hive.druid.metadata.password=${DRUID_PASSWORD};
+#SET hive.druid.metadata.uri=jdbc:mysql://dzaratsian2.field.hortonworks.com/druid;
 SET hive.druid.broker.address.default=druid.example.com:8082;
 CREATE EXTERNAL TABLE if not exists cryptocurrency_market_data
 STORED BY 'org.apache.hadoop.hive.druid.DruidStorageHandler'
 TBLPROPERTIES ("druid.datasource" = "cryptocurrency-market-data");
 EOF
 
-HS2=${1:-localhost:10000}
+HS2=${1:-localhost:10500} #HIVE LLAP HOST:PORT
 BEELINE="beeline -u jdbc:hive2://$HS2/default"
 $BEELINE -f /tmp/create_table.sql

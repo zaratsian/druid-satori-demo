2ca268467cb56318ce095d418d6e795f63d2ddf3
Dan Zaratsian <dtz001@gmail.com>
2018-04-29 08:06:54 -0400

    Update 04query_rest.sh

--- 04query_rest.sh
+++ 04query_rest.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-QUERY=${1:-queries/topn_query.json}
+curl -X POST 'http://dzaratsian1.field.hortonworks.com:8082/druid/v2/?pretty' -H 'content-type: application/json' -d@queries/topn_query.json
 
-curl -X POST 'http://localhost:8082/druid/v2/?pretty' -H 'content-type: application/json' -d@${QUERY}
+#curl -X POST 'http://dzaratsian1.field.hortonworks.com:8082/druid/v2/?pretty' -H 'content-type: application/json' -d@queries/timeseries_query.json
+

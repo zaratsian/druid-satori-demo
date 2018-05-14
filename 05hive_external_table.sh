b96103aec895718823d5816170c8650759494244
Dan Zaratsian <dtz001@gmail.com>
2018-05-14 10:25:31 -0400

    Update 05hive_external_table.sh

--- 05hive_external_table.sh
+++ 05hive_external_table.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # NOTE: This does not work with Derby (use Druid with MySQL or Postgresql)
+# DRUID_USERNAME=druid
+# DRUID_PASSOWRD=horton.Mysql123
 
 cat<<EOF>/tmp/create_table.sql
 SET hive.druid.metadata.username=${DRUID_USERNAME};
@@ -13,12 +15,11 @@ STORED BY 'org.apache.hadoop.hive.druid.DruidStorageHandler'
 TBLPROPERTIES ("druid.datasource" = "cryptocurrency_market_data");
 EOF
 
+# Connect to Hive LLAP (Hive Interactive)
 BEELINE="beeline -u jdbc:hive2://dzaratsian0.field.hortonworks.com:10500/default"
 $BEELINE -f /tmp/create_table.sql
 
-
 #beeline -u jdbc:hive2://dzaratsian0.field.hortonworks.com:10500/default -e "select * from cryptocurrency_market_data limit 10";
-
 
 #beeline -u jdbc:hive2://dzaratsian0.field.hortonworks.com:10500/default -e "select cryptocurrency, count(*) as count from cryptocurrency_market_data group by cryptocurrency";
 

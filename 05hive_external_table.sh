be5b837ccded9f9babbb5030ffeb3b5e7b96a925
Dan Zaratsian <dtz001@gmail.com>
2018-05-14 11:53:01 -0400

    Update 05hive_external_table.sh

--- 05hive_external_table.sh
+++ 05hive_external_table.sh
@@ -7,7 +7,7 @@
 cat<<EOF>/tmp/create_table.sql
 SET hive.druid.metadata.username=${DRUID_USERNAME};
 SET hive.druid.metadata.password=${DRUID_PASSWORD};
-SET hive.druid.broker.address.default=druid.example.com:8082;
+SET hive.druid.broker.address.default=${DRUID_BROKER};
 #SET hive.druid.metadata.uri=jdbc:mysql://dzaratsian2.field.hortonworks.com/druid;
 #SET hive.druid.coordinator.address.default=druid.example.com:8081;
 CREATE EXTERNAL TABLE if not exists cryptocurrency_market_data

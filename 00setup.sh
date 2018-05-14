b27152d74b35fcf8915f2a274f1d04962783bf59
Dan Zaratsian <dtz001@gmail.com>
2018-05-14 11:53:49 -0400

    Update 00setup.sh

--- 00setup.sh
+++ 00setup.sh
@@ -8,6 +8,7 @@ export DRUID_USERNAME=druid
 export DRUID_PASSWORD=horton.Mysql123
 export DRUID_BROKER=dzaratsian5.field.hortonworks.com:8082
 export KAFAK_BROKER=dzaratsian1.field.hortonworks.com:6667
+export HIVE_LLAP_HOST=dzaratsian0.field.hortonworks.com:10500
 
 
 #########################################################################

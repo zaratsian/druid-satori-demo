75885c11eec049d9c8cf57c32a2050dd36fea944
Dan Zaratsian <dtz001@gmail.com>
2018-05-14 11:51:02 -0400

    Update 00setup.sh

--- 00setup.sh
+++ 00setup.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+
+#########################################################################
+# ENV Vars
+#########################################################################
+export DRUID_USERNAME=druid
+export DRUID_PASSWORD=horton.Mysql123
+export DRUID_BROKER=dzaratsian5.field.hortonworks.com:8082
+export KAFAK_BROKER=dzaratsian1.field.hortonworks.com:6667
+
+
+#########################################################################
+# Python Dependencies
+#########################################################################
 #sudo yum install -y python-pip
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python get-pip.py

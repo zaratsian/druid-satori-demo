e36a7bad8451918c170e5e23a4e7af1ab2c67182
Dan Zaratsian <dtz001@gmail.com>
2018-05-23 09:58:14 -0400

    Update 03start_supervisor.sh

--- 03start_supervisor.sh
+++ 03start_supervisor.sh
@@ -1,2 +1,2 @@
 # Start a supervisor task.
-curl -X POST -H 'Content-Type: application/json' -d @supervisor-spec-fixed.json http://druid.example.com:8090/druid/indexer/v1/supervisor
+curl -X POST -H 'Content-Type: application/json' -d @supervisor-spec-fixed.json http://$DRUID_BROKER/druid/indexer/v1/supervisor

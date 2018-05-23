bb9fbb872155ac23b04da85cf2403de3e4eb08d9
Dan Zaratsian <dtz001@gmail.com>
2018-05-23 09:55:03 -0400

    Update 00setup.sh

--- 00setup.sh
+++ 00setup.sh
@@ -11,6 +11,17 @@ export DRUID_BROKER=dzaratsian5.field.hortonworks.com:8082
 export KAFAK_BROKER=dzaratsian1.field.hortonworks.com:6667
 export HIVE_LLAP_HOST=dzaratsian0.field.hortonworks.com:10500
 
+echo ""
+echo ""
+echo "Set the following env variables..."
+sleep 5
+echo ""
+echo "DRUID_USERNAME = " $DRUID_USERNAME
+echo "DRUID_PASSWORD = " $DRUID_PASSWORD
+echo "DRUID_BROKER = " $DRUID_BROKER
+echo "KAFAK_BROKER = " $KAFAK_BROKER
+echo "HIVE_LLAP_HOST = " $HIVE_LLAP_HOST
+
 
 #########################################################################
 # Python Dependencies
@@ -20,3 +31,6 @@ curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python get-pip.py
 
 sudo pip install kafka pykafka satori-rtm-sdk backports.ssl PyOpenSSL 
+
+
+#ZEND
